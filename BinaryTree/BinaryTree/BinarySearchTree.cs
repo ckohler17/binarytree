9bc8a482c787d010ec7687c831b82f3f4e9937fa
Carolynn Kohler <ckohler@gogsf.com>
2019-11-11 14:15:29 -0600

    initial commit. added classes put in some member variables

---
+++ BinaryTree/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinarySearchTree
+    {
+        Node root;
+
+        public BinarySearchTree()
+        {
+
+        }
+
+        public void Add(int data)
+        {
+            Node node = new Node(data);
+            if(root == null)
+            {
+                root = node;
+                return;
+            }
+            //if data > current node add node to the right
+            //if data < current node add node to the left
+        }
+        public void Search()
+        {
+
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+}
